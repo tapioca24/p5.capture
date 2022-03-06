3c5601b06330cb808d2ce7a17069942c1b635551
tapioca24 <h.mitani24@gmail.com>
2022-03-06 10:28:45 +0900

    remove date-fns

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,19 @@
-import { format } from "date-fns";
-
-const DATETIME_FORMAT = "yyyyMMdd-HHmmss";
+export const formatDateTime = (date: Date) => {
+  const years = date.getFullYear();
+  const months = (date.getMonth() + 1).toString().padStart(2, "0");
+  const days = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  return `${years}${months}${days}-${hours}${minutes}${seconds}`;
+};
 
 export const getDirname = (date: Date) => {
-  return format(date, DATETIME_FORMAT);
+  return formatDateTime(date);
 };
 
 export const getFilename = (date: Date, ext: string) => {
-  const datetime = format(date, DATETIME_FORMAT);
+  const datetime = formatDateTime(date);
   return `${datetime}.${ext}`;
 };
 
