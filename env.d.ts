e5e3a79f535a9c1491152e0f170a580ae7abe537
tapioca24 <h.mitani24@gmail.com>
2025-04-26 12:27:46 +0900

    support p5.js v2

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="p5/global" />
+
+type Lifecycles = Record<
+  "presetup" | "postsetup" | "predraw" | "postdraw" | "remove",
+  () => void
+>;
+
+type Addon = (p5: any, fn: any, lifecycles: Lifecycles) => void;
+
+declare module p5 {
+  function registerAddon(addon: Addon): void;
+}
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,23 @@ import { P5Capture } from "@/p5.capture";
 
 const p5Capture = new P5Capture();
 
-p5.prototype.registerMethod("init", function (this: any) {
-  const p5 = this;
-  p5Capture.initialize.call(p5Capture, p5);
-});
-p5.prototype.registerMethod("post", p5Capture.postDraw.bind(p5Capture));
+if (p5.registerAddon) {
+  // p5.js v2
+  const myAddon: Addon = function (_, __, lifecycles) {
+    lifecycles.postsetup = function () {
+      const p5 = this;
+      p5Capture.initialize.call(p5Capture, p5);
+    };
+    lifecycles.postdraw = p5Capture.postDraw.bind(p5Capture);
+  };
+  p5.registerAddon(myAddon);
+} else {
+  // p5.js v1
+  p5.prototype.registerMethod("init", function (this: any) {
+    const p5 = this;
+    p5Capture.initialize.call(p5Capture, p5);
+  });
+  p5.prototype.registerMethod("post", p5Capture.postDraw.bind(p5Capture));
+}
 
 Object.assign(window, { P5Capture });
